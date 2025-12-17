605c1ec833d095553539e256aee77806a073b4b9
paul-shannon <paul.thurmond.shannon@gmail.com>
2025-12-16 21:35:35 -0800

    nose and hat

--- explore/bpyScripts/snowman/snowman.py
+++ explore/bpyScripts/snowman/snowman.py
@@ -1,5 +1,6 @@
 import bpy
 import pdb
+import math
 
 def clearScene():
    bpy.ops.object.select_all(action='SELECT')
@@ -25,17 +26,61 @@ def addSnowMaterial(obj):
    mat.node_tree.links.new(bsdf.outputs['BSDF'], output.inputs['Surface'])
    obj.data.materials.append(mat)
 
+def addNoseMaterial(obj):
+   mat = bpy.data.materials.new(name="NoseMaterial")
+   mat.use_nodes = True
+   nodes = mat.node_tree.nodes
+   nodes.clear()
+   bsdf = nodes.new(type='ShaderNodeBsdfPrincipled')
+   bsdf.inputs['Base Color'].default_value = (1, 0.4, 0, 1.0)
+   bsdf.inputs['Roughness'].default_value = 0.3
+   output = nodes.new(type='ShaderNodeOutputMaterial')
+   mat.node_tree.links.new(bsdf.outputs['BSDF'], output.inputs['Surface'])
+   obj.data.materials.append(mat)
+
+def addHatMaterial(obj):
+   mat = bpy.data.materials.new(name="HatMaterial")
+   mat.use_nodes = True
+   nodes = mat.node_tree.nodes
+   nodes.clear()
+   bsdf = nodes.new(type='ShaderNodeBsdfPrincipled')
+   bsdf.inputs['Base Color'].default_value = (0, 0, 0, 1.0)
+   bsdf.inputs['Roughness'].default_value = 0.3
+   output = nodes.new(type='ShaderNodeOutputMaterial')
+   mat.node_tree.links.new(bsdf.outputs['BSDF'], output.inputs['Surface'])
+   obj.data.materials.append(mat)
+
+    
 def addHat():
-   bpy.ops.mesh.primitive_cylinder_add(location=(0,0,3.46), scale=(0.9, 0.9, 0.05))
+   bpy.ops.mesh.primitive_cylinder_add(location=(0,0,3.40),
+                                       scale=(0.9, 0.9, 0.05))
    hatBrim = bpy.context.active_object
    hatBrim.name = "hatBrim"
 
-   bpy.ops.mesh.primitive_cylinder_add(location=(0,0,3.9), scale=(0.54, 0.54, 0.3))
+   bpy.ops.mesh.primitive_cylinder_add(location=(0,0,3.7),
+                                       scale=(0.54, 0.54, 0.3))
    stovepipe = bpy.context.active_object
    stovepipe.name = "stovepipe"
 
    return (stovepipe, hatBrim)
 
+
+def addNose():
+   bpy.ops.mesh.primitive_cone_add(location=(0.8, 0.0, 3.0),
+                                    rotation=(0.0, math.radians(90.0), 0.0),
+                                    radius1=0.1,
+                                    radius2=0,
+                                    depth=0.8,
+                                    vertices=16
+                                    )
+   nose = bpy.context.active_object
+   nose.name = "nose"
+   return nose
+
+   #bpy.ops.mesh.primitive_cone_add(location=(1,0,3),
+   #                                scale=(0.1, 1, 10),
+   #                                rotation=(0,90,0))
+    
 def createSnowman():
     clearScene()
     body = createSnowball(1.0, 0, 0, 1, "body")
@@ -44,7 +89,11 @@ def createSnowman():
     addSnowMaterial(body)
     addSnowMaterial(torso)
     addSnowMaterial(head)
-    addHat()
+    (stovepipe, hatBrim) = addHat()
+    addHatMaterial(stovepipe)
+    addHatMaterial(hatBrim)
+    nose = addNose()
+    addNoseMaterial(nose)
     return [body, torso, head]
 
 clearScene()
